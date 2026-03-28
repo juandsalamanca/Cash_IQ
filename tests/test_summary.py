1f0bc928fdeb782c2be177e8a2a426ee612676b5
juandsalamanca <juan.salamanca@aidevlab.com>
2026-03-28 12:42:10 -0500

    Test bug fix

--- tests/test_summary.py
+++ tests/test_summary.py
@@ -2,8 +2,11 @@ from src.ai_summary import get_summary
 
 
 def test_summary():
-    date_strt = "2026-02-16"
-    OUTPUT_XLSX = "tests/trinity/output.xlsx"
-    client = "Trinity"
+
+    # Client has to be continuum, luna or strivewell so that in the deployment runtime those tests run beofre this one
+    # That way the output file gets saved and we can use it here
+    date_strt = "2026-03-02"
+    OUTPUT_XLSX = "tests/strivewell/output.xlsx"
+    client = "Strivewell"
     summary_bytes = get_summary(client, date_strt, OUTPUT_XLSX)
     assert summary_bytes[:2] == b'PK'
